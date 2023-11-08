2ccafbaa4608b11cfba21566cdc1372188c5d519
blittle <blittle@uphp.com>
2023-11-08 16:14:50 -0500

    mocking CIL structure

--- src/CIL.fs
+++ src/CIL.fs
@@ -5,8 +5,6 @@ open System.Linq
 open Mono.Cecil
 open Mono.Cecil.Cil
 
-
-
 //-------------------------------------------------------------------------
 // CIL generation helpers
 /// <summary>This function makes a static method with one argument with the corresponding types</summary>
@@ -37,82 +35,84 @@ let endIl (func: ILProcessor) =
 let func n (program: TypeDefinition) = 
     program.Methods.Where(fun m -> m.Name = n).First()
 
-
-let main_il = ()
+let initEnv () = ()
+let completeEnv () = ()
+let saveAssembly () = ()
 
 //--------------------------------------------------------------------------------------------------------------------
 // CIL Assembly generation testing
+//make an assembly
+let appAsm =
+    AssemblyDefinition.CreateAssembly(
+        new AssemblyNameDefinition("CodeGen", new Version(1, 0, 0, 0)),
+        "CodeGen",
+        ModuleKind.Console
+    )
+// get the module from the assembly
+let appModule = appAsm.MainModule
+
+// create the program type(class) and add it to the module
+let programType =
+    new TypeDefinition(
+        "CodeGen",
+        "Program",
+        TypeAttributes.Class ||| TypeAttributes.Public,
+        appModule.TypeSystem.Object
+    )
+appModule.Types.Add(programType)
+let get_func n = func n programType
+
+// add an empty constructor for the program type
+let ProgramCtor =
+    new MethodDefinition(
+        ".ctor",
+        MethodAttributes.Public
+        ||| MethodAttributes.HideBySig
+        ||| MethodAttributes.SpecialName
+        ||| MethodAttributes.RTSpecialName,
+        appModule.TypeSystem.Void
+    )
+
+// create the constructor's method body
+let ProgramCtorIl = ProgramCtor.Body.GetILProcessor()
+ProgramCtorIl.Emit(OpCodes.Ldarg_0)
+ProgramCtorIl.Emit(OpCodes.Call, appModule.ImportReference(typeof<obj>.GetConstructor([||])))
+ProgramCtorIl.Emit(OpCodes.Nop)
+ProgramCtorIl.Emit(OpCodes.Ret)
+programType.Methods.Add(ProgramCtor)
+
+
+//-------------------------------------------------------------------------
+// define an add9 method and add it to 'Program'
+let add9Method =
+    makeFunc "add9" appModule.TypeSystem.Int32 appModule.TypeSystem.Int32 programType
+
+let add9Il = add9Method.Body.GetILProcessor()
+
+add9Il.Emit(OpCodes.Ldc_I4, 9)
+add9Il.Emit(OpCodes.Ldarg_0)
+add9Il.Emit(OpCodes.Add)
+add9Il.Emit(OpCodes.Ret)
+
+//-------------------------------------------------------------------------
+// define the 'Main' method and add it to 'Program'
+let mainMethod =
+    makeFunc "Main" (appModule.ImportReference typeof<string[]>) appModule.TypeSystem.Int32 programType
+
+// start defining the method body
+let mainIl = mainMethod.Body.GetILProcessor()
+mainIl.Emit(OpCodes.Nop)
+mainIl.Emit(OpCodes.Ldstr, "generic method creation woohoo")
+// call the method
+mainIl.Emit(OpCodes.Call, appModule.ImportReference(typeof<Console>.GetMethod("WriteLine", [| typeof<string> |])))
+mainIl.Emit(OpCodes.Ldc_I4, 10)
+mainIl.Emit(OpCodes.Call, appModule.ImportReference(get_func "add9"))
+endIl mainIl
+
+// set the entry point and save the module
+printfn "creating main method"
+appAsm.EntryPoint <- mainMethod
+
 let make_test_app () =
-    //make an assembly
-    let appAsm =
-        AssemblyDefinition.CreateAssembly(
-            new AssemblyNameDefinition("CodeGen", new Version(1, 0, 0, 0)),
-            "CodeGen",
-            ModuleKind.Console
-        )
-    // get the module from the assembly
-    let module_ = appAsm.MainModule
-
-    // create the program type(class) and add it to the module
-    let programType =
-        new TypeDefinition(
-            "CodeGen",
-            "Program",
-            TypeAttributes.Class ||| TypeAttributes.Public,
-            module_.TypeSystem.Object
-        )
-    module_.Types.Add(programType)
-    let get_func n = func n programType
-
-    // add an empty constructor for the program type
-    let ctor =
-        new MethodDefinition(
-            ".ctor",
-            MethodAttributes.Public
-            ||| MethodAttributes.HideBySig
-            ||| MethodAttributes.SpecialName
-            ||| MethodAttributes.RTSpecialName,
-            module_.TypeSystem.Void
-        )
-
-    // create the constructor's method body
-    let il = ctor.Body.GetILProcessor()
-    il.Emit(OpCodes.Ldarg_0)
-    il.Emit(OpCodes.Call, module_.ImportReference(typeof<obj>.GetConstructor([||])))
-    il.Emit(OpCodes.Nop)
-    il.Emit(OpCodes.Ret)
-    programType.Methods.Add(ctor)
-
-
-    //-------------------------------------------------------------------------
-    // define an add9 method and add it to 'Program'
-    let add9Method =
-        makeFunc "add9" module_.TypeSystem.Int32 module_.TypeSystem.Int32 programType
-
-    let add9 = add9Method.Body.GetILProcessor()
-
-    add9.Emit(OpCodes.Ldc_I4, 9)
-    add9.Emit(OpCodes.Ldarg_0)
-    add9.Emit(OpCodes.Add)
-    add9.Emit(OpCodes.Ret)
-
-    //-------------------------------------------------------------------------
-    // define the 'Main' method and add it to 'Program'
-    let mainMethod =
-        makeFunc "Main" (module_.ImportReference typeof<string[]>) module_.TypeSystem.Int32 programType
-
-    // start defining the method body
-    let main = mainMethod.Body.GetILProcessor()
-    main.Emit(OpCodes.Nop)
-    main.Emit(OpCodes.Ldstr, "generic method creation woohoo")
-    // call the method
-    main.Emit(OpCodes.Call, module_.ImportReference(typeof<Console>.GetMethod("WriteLine", [| typeof<string> |])))
-    main.Emit(OpCodes.Ldc_I4, 10)
-    main.Emit(OpCodes.Call, module_.ImportReference(get_func "add9"))
-    endIl main
-
-    // set the entry point and save the module
-    printfn "creating main method"
-    appAsm.EntryPoint <- mainMethod
     printfn "Saving..."
     appAsm.Write("Main.dll")
