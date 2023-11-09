cffaef0d2931ffc7c6b7811c6b111ae0a6f82710
blittle <blittle@uphp.com>
2023-11-09 15:03:26 -0500

    testing more

--- src/CodeGen.fs
+++ src/CodeGen.fs
@@ -1,4 +1,5 @@
 module CodeGen
+
 open Mono.Cecil.Cil
 
 module A = Ast
@@ -10,11 +11,12 @@ module C = CIL
 
 module Locals =
     type t = (string * string) list
-    let empty : t = []
-    let get table search = 
-        List.find (fun (k, v) ->  k = search) table |> snd
-    let enter k v table =
-        (k,v)::table
+    let empty: t = []
+
+    let get table search =
+        List.find (fun (k, v) -> k = search) table |> snd
+
+    let enter k v table = (k, v) :: table
 
 let rec eval_exp =
     // this function needs access to the the ilGenerator for the containing method
@@ -25,82 +27,77 @@ let rec eval_exp =
         // emit ldc_I4 (eval_exp env e1)
         // emit ldc_I4 (eval_exp env e2)
         // emit add
-        | A.Plus -> 
+        | A.Plus ->
             eval_exp e1
             eval_exp e2
             printfn "add"
-        | A.Minus -> 
+        | A.Minus ->
             eval_exp e1
             eval_exp e2
             printfn "sub"
-        | A.Times -> 
+        | A.Times ->
             eval_exp e1
             eval_exp e2
             printfn "mul"
 
-        | A.Neq -> 
+        | A.Neq ->
             eval_exp e1
             eval_exp e2
             printfn "ceq" // 1 if equal
             printfn "ldc_I4 0"
             printfn "ceq"
-        | A.Eq -> 
+        | A.Eq ->
             eval_exp e1
             eval_exp e2
             printfn "ceq" // 1 if equal
-        | A.LT -> 
+        | A.LT ->
             eval_exp e1
             eval_exp e2
-            printfn "clt" 
-        | A.LTE -> 
-            eval_exp e1
-            printfn "stloc 0" //store left operand
-            eval_exp e2
-            printfn "stloc 1" //store right operand
-            printfn "ldloc 0" //load both operands
-            printfn "ldloc 1"
-            printfn "clt"  //compare less than (leaves comparison on stack)
-            printfn "ldloc 0"
-            printfn "ldloc 1"
-            printfn "ceq"  //compare equal (leaves comparison on stack)
-            // need the top of the stack to be 1,1
-            printfn "ldc_I4 1"
-            printfn "ceq" //make sure that both comparisons were positive 
-            printfn "ceq" 
-        | A.GT -> 
-            eval_exp e1
-            eval_exp e2
-            printfn "clt" 
-        | A.GTE -> 
+            printfn "clt"
+        | A.LTE ->
             eval_exp e1
             printfn "stloc 0" //store left operand
             eval_exp e2
             printfn "stloc 1" //store right operand
             printfn "ldloc 0" //load both operands
             printfn "ldloc 1"
-            printfn "cgt"  //compare less than (leaves comparison on stack)
+            printfn "clt" //compare less than (leaves comparison on stack)
             printfn "ldloc 0"
             printfn "ldloc 1"
-            printfn "ceq"  //compare equal (leaves comparison on stack)
+            printfn "ceq" //compare equal (leaves comparison on stack)
             // need the top of the stack to be 1,1
             printfn "ldc_I4 1"
-            printfn "ceq" //make sure that both comparisons were positive 
-            printfn "ceq" 
+            printfn "ceq" //make sure that both comparisons were positive
+            printfn "ceq"
+        | A.GT ->
+            eval_exp e1
+            eval_exp e2
+            printfn "clt"
+        | A.GTE ->
+            eval_exp e1
+            printfn "stloc 0" //store left operand
+            eval_exp e2
+            printfn "stloc 1" //store right operand
+            printfn "ldloc 0" //load both operands
+            printfn "ldloc 1"
+            printfn "cgt" //compare less than (leaves comparison on stack)
+            printfn "ldloc 0"
+            printfn "ldloc 1"
+            printfn "ceq" //compare equal (leaves comparison on stack)
+            // need the top of the stack to be 1,1
+            printfn "ldc_I4 1"
+            printfn "ceq" //make sure that both comparisons were positive
+            printfn "ceq"
 
-    | A.NumExp(i) -> 
-        printfn $"ldc_I4 {i}"
-    | A.BoolExp(b) ->
-        printfn $"ldc_I4 {if b then 1 else 0}"
+    | A.NumExp(i) -> printfn $"ldc_I4 {i}"
+    | A.BoolExp(b) -> printfn $"ldc_I4 {if b then 1 else 0}"
 
-    | A.IfExp(if', then', else') -> 
-        // to simply compile a conditional, we create a label(instruction) for the else branch 
-        // and the end. The end instruction doesn't return, it just leaves the result of the expression 
+    | A.IfExp(if', then', else') ->
+        // to simply compile a conditional, we create a label(instruction) for the else branch
+        // and the end. The end instruction doesn't return, it just leaves the result of the expression
         // at the top of the stack
         // we can use a small assoc list for the labeled opcodes
-        let labels = 
-            Locals.empty
-            |> Locals.enter "else" "nop"
-            |> Locals.enter "end" "nop"
+        let labels = Locals.empty |> Locals.enter "else" "nop" |> Locals.enter "end" "nop"
 
         printfn $"making label else: {eval_exp else'}" // let lables = ("else", il.Create(Nop))::labels
         printfn $"making label end: ldloc 1" // let lables = ("end", il.Create(Ldloc, 1))::labels
@@ -120,8 +117,12 @@ let rec eval_exp =
         printfn "%s" <| Locals.get labels "end"
         printfn $"ldloc 1"
 
-    | A.CallExp(name, arg) -> printfn "unimplemented" 
-    | A.IdExp(name) -> printfn "unimplemented" 
+    | A.CallExp(name, arg) ->
+        printfn "ldc_I4 arg"
+        printfn "call name"
+    | A.IdExp(name) ->
+        printfn "ldarg.0"
+        printfn "ldfld type name"
 
 let eval_dec =
     // this function needs access to the main class's ilGenerator
@@ -129,26 +130,32 @@ let eval_dec =
     | A.FunDec(binding, fundec) -> // makeFunc at the top level
         // easy to make function type from this (type list) but how do we call it?
         // make the type returned be abstract
-        // TODO make recursive functions work
         let { A.outtype = outtype
-              A.arg = { A.argname = argname; A.type' = intype }
+              A.arg = { A.argname = argname
+                        A.type' = intype }
               A.body = body } =
             fundec
-
+        //call make function
         printfn "not implemented"
 
     | A.VarDec(binding, value) -> // stloc at the top level
         // associate the binding with the result of this exp in the env
+        // TODO make a function to initialize all of the globals in the Program constructor
+        let name =
+            match binding with
+            | A.Val x -> x
+            | _ -> failwith "expecting a variable binding"
 
-        printfn "not implemented"
+        printfn "ldarg.0"
+        eval_exp value
+        printfn "stfld type name"
 
 let eval_stm =
     function
-    | A.Dec(d) -> printfn "unimplemented"
-    | A.Exp(e) -> eval_exp e  
+    | A.Dec(d) -> eval_dec d
+    | A.Exp(e) -> eval_exp e
 
 let eval_prog =
     function
     | A.Prog(stms) -> List.iter (eval_stm) stms
     | A.Blank -> failwith "No Statements in program"
-
